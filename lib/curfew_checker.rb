8b6e8ee448a843457defe836ba3e16856cffa0b4
Alex Cowan <alexandria.cowan.17@tfacorps.org>
2018-07-10 00:48:25 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,5 +1,7 @@
 def simple_curfew_checker(time)
-  if time >= 11
+  if time > 11
+    puts "You're in trouble! Better get home quick!"
+  elsif time == 11
     puts "You're in trouble! Better get home quick!"
   else time < 11
     puts "You can keep having fun"
